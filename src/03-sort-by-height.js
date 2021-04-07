9dd1c076f177edeb82dccc124517ff20698d5fc2
DevAlexeyProtasenya <283fg56j98@gmail.com>
2021-04-07 21:47:42 +0300

    feat: add "sort by height" function

--- src/03-sort-by-height.js
+++ src/03-sort-by-height.js
@@ -9,8 +9,21 @@
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new Error('Not implemented');
+function sortByHeight(arr) {
+  const sorted = new Array(0);
+  arr.forEach((element) => {
+    if (element !== -1) {
+      sorted.push(element);
+    }
+  });
+  sorted.sort((a, b) => a - b);
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== -1) {
+      // eslint-disable-next-line no-param-reassign
+      arr[i] = sorted.shift();
+    }
+  }
+  return arr;
 }
 
 module.exports = sortByHeight;
