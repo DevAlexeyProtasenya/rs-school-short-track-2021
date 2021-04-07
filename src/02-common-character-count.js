3de5e1182a5f577c8d3dea229f1f953e0f8c7e0e
DevAlexeyProtasenya <283fg56j98@gmail.com>
2021-04-07 16:17:15 +0300

    feat: add solutions of task 01 and 02

--- src/02-common-character-count.js
+++ src/02-common-character-count.js
@@ -9,8 +9,23 @@
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new Error('Not implemented');
+function getCommonCharacterCount(s1, s2) {
+  const map = new Map();
+  let result = 0;
+  s1.split('').forEach((element) => {
+    if (map.get(element) === undefined) {
+      map.set(element, 1);
+    } else {
+      map.set(element, map.get(element) + 1);
+    }
+  });
+  s2.split('').forEach((element) => {
+    if (map.get(element) >= 1 && map.get(element) !== undefined) {
+      result++;
+      map.set(element, map.get(element) - 1);
+    }
+  });
+  return result;
 }
 
 module.exports = getCommonCharacterCount;
