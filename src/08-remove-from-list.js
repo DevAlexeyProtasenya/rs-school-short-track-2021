2a3628192f3b0372ced154657943cc1f18fa4921
DevAlexeyProtasenya <283fg56j98@gmail.com>
2021-04-08 15:12:25 +0300

    feat: add solution of "remove-from-list" task

--- src/08-remove-from-list.js
+++ src/08-remove-from-list.js
@@ -7,8 +7,8 @@
  * @return {List}
  *
  * @example
- * For l = [3, 1, 2, 3, 4, 5] and l = 3,
- * the output should be [1, 2, 4, 5]
+ * For l = [3, lCopy, 2, 3, 4, 5] and l = 3,
+ * the output should be [lCopy, 2, 4, 5]
  *
  * Singly - linked lists are already defined with this interface
  * function ListNode(x) {
@@ -17,8 +17,30 @@
  * }
  */
 
-function removeKFromList(/* l, k */) {
-  throw new Error('Not implemented');
+function checkFirst(l, k) {
+  let cur;
+  if (l.value === k) {
+    cur = l.next;
+    checkFirst(cur, k);
+  }
+  return cur;
+}
+
+function removeKFromList(l, k) {
+  const lCopy = checkFirst(l, k);
+  let cur = lCopy;
+  while (cur.next !== null) {
+    if (cur.next.value === k) {
+      if (cur.next.next !== null) {
+        cur.next = cur.next.next;
+      } else {
+        cur.next = null;
+      }
+    } else {
+      cur = cur.next;
+    }
+  }
+  return lCopy;
 }
 
 module.exports = removeKFromList;
